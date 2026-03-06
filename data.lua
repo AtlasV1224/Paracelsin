78a64513c48ceed6c4ffc8ab3c961b74a2da0f18
AtlasV <atlasvpersonal@gmail.com>
2026-03-06 18:57:22 +0000

    [Fix] Fixed incompatibile zinc rivets and zinc solder recipes for mods that add multiple characters (Multiple Characters mod)

--- data.lua
+++ data.lua
@@ -92,11 +92,12 @@ data.raw.item["accumulator-v2"].default_import_location = "paracelsin"
 data.raw.item["accumulator-v2"].weight = 100000
 
 local function add_player_crafting_categories(categories)
-    local entity = data.raw.character.character
+  for _, entity in pairs(data.raw.character) do  -- iterate all characters
     for _,category in pairs(categories) do
       table.insert(entity.crafting_categories, category)
-    end
+    end 
   end
+end
   
   add_player_crafting_categories({"hand-crafting"})
 
